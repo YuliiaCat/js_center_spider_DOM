7c7a9e752351948df0aa2169553362c8fb818340
Yuliia <kotyuliya24@gmail.com>
2024-05-07 17:46:57 -0400

    make build

---
+++ index.6c7b965a.js
@@ -0,0 +1,2 @@
+var t=document.querySelector(".spider"),e=t.offsetHeight,c=t.offsetWidth;t.style.top="calc(50% - ".concat(e/2,"px)"),t.style.left="calc(50% - ".concat(c/2,"px)");
+//# sourceMappingURL=index.6c7b965a.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,8 @@
+"use strict";
+const spider = document.querySelector(".spider");
+const spiderHeight = spider.offsetHeight;
+const spiderWidth = spider.offsetWidth;
+spider.style.top = `calc(50% - ${spiderHeight / 2}px)`;
+spider.style.left = `calc(50% - ${spiderWidth / 2}px)`;
+
+//# sourceMappingURL=index.f75de5e1.js.map
